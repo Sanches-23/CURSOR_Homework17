c00d6c91d5f213c45f1d6db09f891df47f97d1ca
Sanches-23 <pasechnik23.2002@gmail.com>
2023-08-11 12:00:26 +0300

    hw17

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>HE-HE</h1>
+        {/*<a*/}
+        {/*  className="App-link"*/}
+        {/*  href="https://reactjs.org"*/}
+        {/*  target="_blank"*/}
+        {/*  rel="noopener noreferrer"*/}
+        {/*>*/}
+        {/*  Learn React*/}
+        {/*</a>*/}
       </header>
     </div>
   );
