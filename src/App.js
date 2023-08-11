6f697f02dfb0d651e0f8859d5d5c74d51970cab4
Sanches-23 <pasechnik23.2002@gmail.com>
2023-08-11 13:03:49 +0300

    hw17 0.2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import baseStyle from './Base.module.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>HE-HE</h1>
-      </header>
-    </div>
+      <div className={baseStyle.mainContainer}>
+        <></>
+
+
+
+
+
+      </div>
   );
 }
 
